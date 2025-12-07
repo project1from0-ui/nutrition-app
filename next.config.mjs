0ce797f427366206039cf24327bbf6031437af94
Ogura-Koyo <oguogukoyo@gmail.com>
2025-12-08 01:27:57 +0900

    fix: enable workers and multiple CPUs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
-  experimental: {
-    // コンパイルタイムアウトを回避
-    workerThreads: false,
-    cpus: 1,
-  },
   // 最適化を無効化してコンパイル速度を優先
   swcMinify: false,
 };
